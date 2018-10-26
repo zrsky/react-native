dfa4d6ada190f5db0604fc5355d5bbf824f4cc40
zrsky <1027106581@qq.com>
2018-10-26 17:54:52 +0800

    first commit

---
+++ components/header/header.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>我是头部</Text>
+      </View>
+    );
+  }
+}
+    
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
